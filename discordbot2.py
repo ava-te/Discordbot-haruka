9dfde30f7a3c9310d89c55e6290058bfe33e617a
ava-te <avatanion@gmail.com>
2021-05-20 14:56:34 +0900

    discordbot.py

--- discordbot2.py
+++ discordbot2.py
@@ -3,11 +3,14 @@ import sys
 import os
 import pickle
 import random
+import datetime
 
 
 client = discord.Client()
-CHANNEL_ID =  # 任意のチャンネルID(int)
+CHANNEL_ID = 831420404286881827 # 任意のチャンネルID(int)
 path = "C:/Users/avatail/Desktop/学習用プロジェクト/discord_bot2/.user_info/"
+datetime_today = f'{datetime.date.today()}'
+user_info_list = []
 
 @client.event
 async def on_ready():
@@ -16,46 +19,40 @@ async def on_ready():
     print(client.user.id)
     print('------')
     channel = client.get_channel(CHANNEL_ID)
-    await channel.send('おはよう！')
-text_path = os.path.join(path,"info.sav")
-try:
-    file = open(text_path,"rb")
-    texten = pickle.load(file)
-    file.close
-except:
-    texten = []
+    await channel.send('おはよう！')      
 
-# リプライを貰うとそのユーザーのSAVファイルが作成されるようにする
 @client.event
-async def reply(message):
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    # メッセージやメンションを貰うとそのユーザーのsavが日付ごとに作成・リストにその内容が追記される。
     if message.content.endswith("セーブファイル作って"):
         random_mention = ["君の事憶えとくね！", "これから憶えとくね！", "記憶力〇", "記憶力◎", "記憶しとくよ～"]
         mention_hentou = random.choice(random_mention)
         reply = f'{message.author.mention}' + mention_hentou 
         await message.channel.send(reply)
         user_info_id = f'{message.author.id}'
-        text_path = os.path.join(path, user_info_id + "info.sav")
+        text_path = os.path.join(path, user_info_id + "date" + datetime_today + "info.sav")        
         file = open(text_path, "wb")
-        pickle.dump(texten,file)
-        file.close()        
-    
-@client.event
-async def on_message(message):
-    if message.author.bot:
-        return
+        pickle.dump(user_info_list,file)
+        file.close()
+    else:
+        user_mention = f'{message.content}'
+        user_info_id = f'{message.author.id}'
+        text_path = path + user_info_id + "date" + datetime_today + "info.sav"
+        file = open(text_path, "wb")
+        user_info_list.append(user_info_id + user_mention)
+        pickle.dump(user_info_list,file)
+        file.close()
+        print(user_info_list)
+
     # 「おはよう」で始まるか調べる
-    if message.content.startswith("おはよう"):
+    if message.content.endswith("おはよう"):
             # メッセージを書きます
             m = "おはようございます" + message.author.name + "さん！"
             # メッセージが送られてきたチャンネルへメッセージを送ります
             await message.channel.send(m)
-
-    #誰かがセーブお願いと発言すると蓄積したデータがtext.savに保存される。
-    if message.content.endswith("セーブお願い"):
-        file =open(text_path, "wb")
-        pickle.dump(texten,file)
-        file.close()
-        await message.channel.send('セーブ完了！')
 
     # ランダム12桁整数
     if message.content.endswith("パスワード作って"): 
@@ -63,13 +60,9 @@ async def on_message(message):
         random12 = str(num_random12)
         await message.channel.send("はいよ！" + random12)
 
-    # 発言時に実行されるイベントハンドラを定義
-    if client.user in message.mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行
-
     if message.content.startswith('!SHUTDOWN_BOT'):#!SHUTDOWN_BOTが入力されたら強制終了
         await client.logout()
         await sys.exit()
 
 # token にDiscordのデベロッパサイトで取得したトークンを入れてください
-client.run("")
+client.run("ODQxMzA4MjEyMTgzODI2NDQ0.YJk3VA.F-5ZkiZjxCZMaCsmMyrYcYqFAOM")
